e894aa21a0874068d96ab020a84b43c7a255c137
Alisha <alisha27011997@gmail.com>
2019-12-13 16:00:33 -0500

    until gridView

---
+++ app/src/main/java/com/example/alisha_765497_ft/Verification.java
@@ -0,0 +1,14 @@
+package com.example.alisha_765497_ft;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Verification extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_verification);
+    }
+}
