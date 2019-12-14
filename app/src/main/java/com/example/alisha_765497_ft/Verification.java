cb69211a5198cd5561cd31c8ec8718b99f690c5e
Alisha <alisha27011997@gmail.com>
2019-12-14 14:13:14 -0500

    Shuffle done

--- app/src/main/java/com/example/alisha_765497_ft/Verification.java
+++ app/src/main/java/com/example/alisha_765497_ft/Verification.java
@@ -30,6 +30,8 @@ public class Verification extends AppCompatActivity {
     int[] image = {R.drawable.img1,R.drawable.img2,R.drawable.img3,R.drawable.img4,
             R.drawable.img5,R.drawable.img6,R.drawable.img7,R.drawable.img8,R.drawable.img9};
 
+
+//    int[] checkArray = {}
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
